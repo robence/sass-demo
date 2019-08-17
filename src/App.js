a5164f56deb4ce5cb85ed85c560bce6168f63d8e
rozsabence <robence@gmail.com>
2019-08-17 20:35:14 +0200

    🎉 create base layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="orange-container">
+        <p>Turn left to go right!</p>
+        <div className="button-group">
+          <button id="left">Left</button>
+          <button id="right">Right</button>
+        </div>
+      </div>
     </div>
   );
 }
