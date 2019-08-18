b36badc15b5e3ead5e102af2ae1b4f32a5d18592
rozsabence <robence@gmail.com>
2019-08-18 15:37:51 +0200

    Updates

---
+++ precache-manifest.e0715741936c88f4400065b31dd2ab66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b570bd76389844517b405c590f1deecc",
+    "url": "/sass-demo/index.html"
+  },
+  {
+    "revision": "b5fc5ddedc4e89474365",
+    "url": "/sass-demo/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "666e7dbd5351429d09fd",
+    "url": "/sass-demo/static/css/main.6511529e.chunk.css"
+  },
+  {
+    "revision": "b5fc5ddedc4e89474365",
+    "url": "/sass-demo/static/js/2.4a2d84bc.chunk.js"
+  },
+  {
+    "revision": "666e7dbd5351429d09fd",
+    "url": "/sass-demo/static/js/main.50b174ca.chunk.js"
+  },
+  {
+    "revision": "af042257c9a9ff60226b",
+    "url": "/sass-demo/static/js/runtime~main.c3fb1345.js"
+  }
+]);
